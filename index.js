796fe496a9aee9d7abbae2550b6be1f37c2ef8b9
Joyt <zhouytforever@gmail.com>
2019-06-30 05:50:53 -0400

    get urls finished

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const ax = require('axios')
+const axios = require('axios')
+const {
+  replacePage,
+  getPageList,
+  getDetailPageImgUrls
+} = require('./utils/common')
+
+const baseURL = 'https://m1.lukkav.com'
+const basePageUrl = '/art/type/id/8/page/$$.html'
+
+let ax = axios.create({ baseURL })
+
+getPageList(ax, replacePage(basePageUrl, 1))
+  .then(async (list) => {
+    for (let i = 0, len = list.length; i < len; i++) {
+      let { imgUrls, title } = await getDetailPageImgUrls(ax, list[i])
+      if (imgUrls === undefined) throw new Error('imgUrls undefined')
+      console.log(title)
+      console.log()
+      console.log(imgUrls)
+      console.log()
+    }
+  })
+  .catch(err => console.log(err))
--- test/first.js
+++ test/first.js
@@ -1,1 +1,10 @@
-console.log('test')
+const expect = require('chai').expect
+const replacePage = require('../utils/common').replacePage
+
+const baseUrl = 'https://m1.lukkav.com/art/type/id/8/page/$$.html'
+
+describe('utils', () => {
+  it('url replace', () => {
+    expect(replacePage(baseUrl, 1)).to.be.equal('https://m1.lukkav.com/art/type/id/8/page/1.html')
+  })
+})
