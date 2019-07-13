6bb1e367ca0d9e0ce99a1256977fbb2a9330c354
Joyt <zhouytforever@gmail.com>
2019-07-12 23:20:28 -0400

    finish craw whithout database

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
-const axios = require('axios')
-const { insertMany, openAlbum } = require('./utils/mongo')
-const {
-  replacePage,
-  getPageList,
-  getDetailPageImgUrls
-} = require('./utils/common')
+// const { insertMany, openAlbum } = require('./utils/mongo')
+const FetchPics = require('./utils/common')
+// openAlbum((album) => insertMany(album, albumSets))
 
-const baseURL = 'https://m1.lukkav.com'
-const basePageUrl = '/art/type/id/8/page/$$.html'
+const baseURL = 'https://m1.luffav.com'
+const firstPageUrl = '/art/type/id/8/page/2.html'
 
-let ax = axios.create({ baseURL })
-
-const getOnePage = (pageNum) => {
-  getPageList(ax, pageNum)
-    .then(async (list) => {
-      let albumSets = []
-      for (let i = 0, len = list.length; i < len; i++) {
-        let { imgUrls, title } = await getDetailPageImgUrls(ax, list[i])
-        if (imgUrls === undefined) throw new Error('imgUrls undefined')
-        let realTitle = /(.*)\s*-/.exec(title)[1]
-        albumSets.push({
-          title: realTitle,
-          pics: imgUrls
-        })
-      }
-      openAlbum((album) => insertMany(album, albumSets))
-    })
-    .catch(err => console.log(err))
+const hasNextPage = ($html, currentPath) => {
+  return !/828\.html/.test(currentPath)
 }
 
-const region = { first: 1, last: 821 }
+const titleProcessor = ($html) => {
+  let temp = $html('title').text()
+  let reg = /(.+)\s-/
+  let result = reg.exec(temp)
+  return result[1] || temp
+}
+const urlProcessor = ($html) => {
+  const $imgs = $html('#wrapper > div:nth-child(6) > p:nth-child(8)  img')
+  let result = []
+  $imgs.each((i, e) => {
+    result.push(e.attribs.src)
+  })
+  return result
+}
 
-const getPages = async (current, last) => {
-  if (current > last) {
-    console.log('完成全部')
-    return null
-  } else {
-    console.log(`加载第 ${current} 页`)
-    await getOnePage(replacePage(basePageUrl, current))
-    getPages(current + 1, last)
+const processor = async (pageUrl) => {
+  try {
+    const fetchPics = new FetchPics(baseURL, pageUrl)
+    const list = await fetchPics.getPageList()
+    let albumList = []
+    for (let i = 0, len = list.length; i < len; i++) {
+      let entity = await fetchPics.getDetailPage(
+        list[i],
+        titleProcessor,
+        urlProcessor
+      )
+      if (entity.key === 'no_imgs') continue
+      albumList.push(entity)
+    }
+    console.log(albumList)
+    if (fetchPics.hasNextPage(hasNextPage)) {
+      let nextPagePath = fetchPics.nextPagePath(($page) =>
+        $page('.page_info  a[title="下一页"]').attr('href'))
+      setTimeout(() => processor(nextPagePath), 1000)
+    }
+  } catch (e) {
+    console.log(e)
   }
 }
-
-getPages(region.first, region.last)
+processor(firstPageUrl)
--- utils/common.js
+++ utils/common.js
@@ -1,30 +1,60 @@
 const $ = require('cheerio')
+const axios = require('axios')
 
-module.exports = {
-  replacePage: (baseUrl, page) => baseUrl.replace('$$', page),
-  getPageList: (ax, url) => ax.get(url)
-    .then((res) => {
-      let evary = []
-      let html = $.load(res.data)
-      html('ul.ui-list > li > a').each(function () {
-        evary.push($(this).attr('href'))
-      })
-      return evary
-    })
-    .catch((err) => {
+const requestLogger = (config) => {
+  console.log('连接：', config.baseURL + config.url)
+  return config
+}
+class FetchPics {
+  constructor (baseURL, firstPath) {
+    this.ax = axios.create({ baseURL })
+    this.ax.interceptors.request.use(requestLogger, (e) => Promise.reject(e))
+    this.$page = null
+    this.path = firstPath
+    this.pageList = []
+  }
+  replacePageURL (baseUrl, page) {
+    return baseUrl.replace('$$', page)
+  }
+  nextPagePath (operator) {
+    this.path = operator(this.$page, this.path)
+    return this.path
+  }
+  hasNextPage (operator) {
+    return operator(this.$page, this.path)
+  }
+  async getPageList (aimElementPath =
+  'ul.ui-list > li > a') {
+    try {
+      const res = await this.ax.get(this.path)
+      this.$page = $.load(res.data)
+      this.pageList = this.$page(aimElementPath)
+        .map((i, e) => e.attribs.href)
+      return this.pageList
+    } catch (err) {
       console.log(err)
-    }),
-  getDetailPageImgUrls: (ax, url) => ax.get(url)
-    .then((res) => {
-      let imgUrls = []
-      let html = $.load(res.data)
-      html('#wrapper > div:nth-child(6) > p:nth-child(8) img').each(function (i, e) {
-        imgUrls.push(e.attribs.src)
-      })
+      throw new Error('getPageList 出错中断')
+    }
+  }
+  async getDetailPage (url,
+    titleProcessor = ($html) => $html('title')[0].text(),
+    urlProcessor = ($html) => ''
+  ) {
+    try {
+      const res = await this.ax.get(url)
+      let $html = $.load(res.data)
+      let imgUrls = urlProcessor($html)
       return {
         imgUrls,
-        title: html('title').text()
+        title: titleProcessor($html),
+        key: Array.isArray(imgUrls) && imgUrls.length > 0
+          ? Buffer.from(imgUrls[0]).toString('base64')
+          : 'no_imgs'
       }
-    })
-    .catch(e => console.log(e))
+    } catch (e) {
+      console.log(e)
+      throw new Error('getDetailPage 出错中断')
+    }
+  }
 }
+module.exports = FetchPics
